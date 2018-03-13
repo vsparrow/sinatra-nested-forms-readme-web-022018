82d632a737db12ac57d29eeda322e671cd71c4c3
vsparrow <khan@vsparrow.com>
2018-03-13 10:42:30 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require './app'
-require_all 'models'
+# require './app/controllers/app.rb'
+# # require_all 'models'
+# require './app/'
+
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/views", "*.erb")].each {|f| require f}
